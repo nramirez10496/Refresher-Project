6e9dc6e5dbf7d65765ada183aec1ba61e61ed077
nramirez10496 <nramirez10496@gmail.com>
2026-09-06 17:15:07 -0500

    Doors open/close animations

---
+++ Assets/Door.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OpenDoor()
+    {
+        animator.SetTrigger("Open");
+    }
+    public void CloseDoor()
+    {
+        animator.SetTrigger("Close");
+
+    }
+}
---
+++ Assets/DoorTrigger.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] UnityEvent OnEnter;
+    [SerializeField] UnityEvent OnExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnEnter?.Invoke();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        OnExit?.Invoke();
+    }
+}
